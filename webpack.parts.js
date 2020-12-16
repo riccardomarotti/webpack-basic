de677eeb84aa2a441c2a857380a6a083b0813eae
Riccardo <riccardo.marotti@memooria.org>
2020-12-16 10:21:15 +0100

    Use PurgeCSS in production

--- webpack.parts.js
+++ webpack.parts.js
@@ -46,6 +46,25 @@ exports.tailwind = () => ({
   },
 });
 
+const path = require("path");
+const glob = require("glob");
+const PurgeCSSPlugin = require("purgecss-webpack-plugin");
+const ALL_FILES = glob.sync(path.join(__dirname, "src/*.js"));
+exports.eliminateUnusedCSS = () => ({
+  plugins: [
+    new PurgeCSSPlugin({
+      paths: ALL_FILES, // Consider extracting as a parameter
+      extractors: [
+        {
+          extractor: (content) =>
+            content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+          extensions: ["html"],
+        },
+      ],
+    }),
+  ],
+});
+
 exports.page = ({ title }) => ({
   plugins: [new MiniHtmlWebpackPlugin({ context: { title } })],
 });
